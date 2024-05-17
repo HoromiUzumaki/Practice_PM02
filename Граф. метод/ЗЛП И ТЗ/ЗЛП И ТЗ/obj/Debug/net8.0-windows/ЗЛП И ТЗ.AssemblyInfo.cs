5baee0dd779e9feae780c17a994da62ee19bab10
HoromiUzumaki <ksushakrotova16@yandex.ru>
2024-05-17 14:40:08 +0300

    1

--- Граф. метод/ЗЛП И ТЗ/ЗЛП И ТЗ/obj/Debug/net8.0-windows/ЗЛП И ТЗ.AssemblyInfo.cs
+++ Граф. метод/ЗЛП И ТЗ/ЗЛП И ТЗ/obj/Debug/net8.0-windows/ЗЛП И ТЗ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ЗЛП И ТЗ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c38dad1a7ba2682dacc526c0ec1d3119ae26df15")]
 [assembly: System.Reflection.AssemblyProductAttribute("ЗЛП И ТЗ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ЗЛП И ТЗ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
